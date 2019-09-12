b9432b7b89dc8962fd58da83c74c3786f5620edf
danielsmith <smithdaniel@live.com>
2019-09-12 13:43:33 -0400

    added form

---
+++ client/src/Form.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import database from './sample_record2'
+
+export default class Form extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            database:[]
+    }
+}
+
+    handleChange = (e)=>{
+        const {name, value} = e.target;
+        this.setState({
+            [name]:value
+        })
+    }
+    
+    
+
+    render() {
+        return (
+            <div>
+                <h3 className="heading2"> Input the Name of the Student!</h3>
+                <form>
+                    <input
+                    name="date"
+                    type="text"
+                    placeholder="Date"
+                    onChange={this.handleChange}
+                    />
+                    <input
+                    name="name"
+                    type="text"
+                    placeholder="Name"
+                    onChange={this.handleChange}
+                    />
+                  
+                    <button>Submit</button>
+                    
+
+                </form>
+            </div>
+        )
+    }
+}
