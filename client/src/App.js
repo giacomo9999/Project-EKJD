b49d033327cbeb07a5ecd4efece56f88d1e0f185
James Gary <gmarchesi888@gmail.com>
2019-09-12 10:09:43 -0400

    Media queries in place too.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
+import "./media-queries.css";
 
 function App() {
   return (
     <div className="container-outer">
-    <h1>I Am The App</h1>
+      <h1>I Am The App</h1>
     </div>
   );
 }
