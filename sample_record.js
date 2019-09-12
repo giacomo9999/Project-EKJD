e52ffdbe7380fb539066aff84b6de71cd9b57dd3
James Gary <gmarchesi888@gmail.com>
2019-09-12 10:47:48 -0400

    Fixed sample data.

--- sample_record.js
+++ sample_record.js
@@ -1,19 +1,56 @@
 const database = {
-  date: 190912,
-  student_data: [
+  data: [
     {
-      id: 1,
-      student_name: "John Doe",
-      adhd: true,
-      depression: false,
-      anxiety: true
+      date: 190912,
+      student_data: [
+        {
+          id: 1,
+          student_name: "John Doe",
+          adhd: true,
+          depression: false,
+          anxiety: true
+        },
+        {
+          id: 2,
+          student_name: "Jane Smith",
+          adhd: true,
+          depression: false,
+          anxiety: true
+        },
+        {
+          id: 3,
+          student_name: "Julio Iglesias",
+          adhd: true,
+          depression: false,
+          anxiety: true
+        }
+      ]
     },
     {
-        id: 2,
-        student_name: "Jane Gonzales",
-        adhd: false,
-        depression: false,
-        anxiety: true
+        date: 190913,
+        student_data: [
+          {
+            id: 1,
+            student_name: "John Doe",
+            adhd: true,
+            depression: false,
+            anxiety: true
+          },
+          {
+            id: 2,
+            student_name: "Jane Smith",
+            adhd: true,
+            depression: false,
+            anxiety: true
+          },
+          {
+            id: 3,
+            student_name: "Julio Iglesias",
+            adhd: true,
+            depression: false,
+            anxiety: true
+          }
+        ]
       }
   ]
 };
