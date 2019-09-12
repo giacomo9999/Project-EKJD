b6d767d347d0dca164e1f779e9dbefe520e9d9ff
James Gary <gmarchesi888@gmail.com>
2019-09-12 10:37:34 -0400

    Added sample db.

---
+++ sample_record.js
@@ -0,0 +1,19 @@
+const database = {
+  date: 190912,
+  student_data: [
+    {
+      id: 1,
+      student_name: "John Doe",
+      adhd: true,
+      depression: false,
+      anxiety: true
+    },
+    {
+        id: 2,
+        student_name: "Jane Gonzales",
+        adhd: false,
+        depression: false,
+        anxiety: true
+      }
+  ]
+};
