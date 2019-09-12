036c987c62a4889d96bf2d2c26405f0423bcf015
James Gary <gmarchesi888@gmail.com>
2019-09-12 11:58:52 -0400

    Building Analyzer.

--- client/src/Analyzer.js
+++ client/src/Analyzer.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 
 class Analyzer extends Component {
+  showStudentsOnDate = dateIn => {
+    const studentsPresent = this.props.data[dateIn].map(entry => {
+      return <li key={entry.id}>{entry.student_name}</li>;
+    });
+    return studentsPresent;
+  };
   render() {
-    const studentsOnDate = this.props.data[this.props.currentDate].map(
-      entry => {
-        return <li key={entry.id}>{entry.student_name}</li>;
-      }
-    );
     console.log(this.props.data);
     return (
       <div className="container-inner">
         <h2>I Am The Data Analyzer</h2>
-        {studentsOnDate}
+        {this.showStudentsOnDate(190912)}
       </div>
     );
   }
