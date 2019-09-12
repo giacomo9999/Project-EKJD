2bd533e34e10fb3fd7cf1ab463d32889d3fdf4ee
danielsmith <smithdaniel@live.com>
2019-09-12 16:01:53 -0400

    added

---
+++ sample-record2.js
@@ -0,0 +1,196 @@
+const database = {
+    190912: [
+    {
+      id: 1,
+      student_name: "Lillie J. Gentry",
+      adhd: true,
+      depression: false,
+      anxiety: true
+    },
+    {
+      id: 2,
+      student_name: "Jeff L. Fletcher",
+      adhd: true,
+      depression: false,
+      anxiety: true
+    },
+    {
+      id: 3,
+      student_name: "Irena J. Davis",
+      adhd: true,
+      depression: false,
+      anxiety: true
+    },
+    {
+        id: 4,
+        student_name: "Lillian M. Razo",
+        adhd: true,
+        depression: false,
+        anxiety: true
+      },
+      {
+        id: 5,
+        student_name: "Laverne W. Murphy",
+        adhd: true,
+        depression: false,
+        anxiety: true
+      },
+      {
+        id: 6,
+        student_name: "Julio Iglesias",
+        adhd: true,
+        depression: false,
+        anxiety: true
+      },
+      {
+        id: 7,
+        student_name: "Frank J. Byard",
+        adhd: true,
+        depression: false,
+        anxiety: true
+      },
+      {
+        id: 8,
+        student_name: "Caleb S. Sun",
+        adhd: true,
+        depression: false,
+        anxiety: true
+      },
+      {
+        id: 9,
+        student_name: "Donna R. Farmer",
+        adhd: true,
+        depression: false,
+        anxiety: true
+      }
+  ],
+  190913: [
+    {
+      id: 1,
+      student_name: "Lillie J. Gentry",
+      adhd: false,
+      depression: true,
+      anxiety: flase
+    },
+    {
+      id: 2,
+      student_name: "Jeff L. Fletcher",
+      adhd: false,
+      depression: true,
+      anxiety: false
+    },
+    {
+      id: 3,
+      student_name: "Irena J. Davis",
+      adhd: true,
+      depression: true,
+      anxiety: false
+    },
+    {
+        id: 4,
+        student_name: "Lillian M. Razo",
+        adhd: false,
+        depression: true,
+        anxiety: true
+      },
+      {
+        id: 5,
+        student_name: "Laverne W. Murphy",
+        adhd: false,
+        depression: true,
+        anxiety: false
+      },
+      {
+        id: 6,
+        student_name: "Julio Iglesias",
+        adhd: false,
+        depression: true,
+        anxiety: false
+      },
+      {
+        id: 7,
+        student_name: "Frank J. Byard",
+        adhd: true,
+        depression: true,
+        anxiety: false
+      },
+      {
+        id: 8,
+        student_name: "Caleb S. Sun",
+        adhd: false,
+        depression: true,
+        anxiety: false
+      },
+      {
+        id: 9,
+        student_name: "Donna R. Farmer",
+        adhd: false,
+        depression: true,
+        anxiety: false
+      }
+  ],
+  190914: [
+    {
+      id: 1,
+      student_name: "Lillie J. Gentry",
+      adhd: true,
+      depression: true,
+      anxiety: false
+    },
+    {
+      id: 2,
+      student_name: "Jeff L. Fletcher",
+      adhd: false,
+      depression: true,
+      anxiety: false
+    },
+    {
+      id: 3,
+      student_name: "Irena J. Davis",
+      adhd: false,
+      depression: true,
+      anxiety: false
+    },
+    {
+        id: 4,
+        student_name: "Lillian M. Razo",
+        adhd: false,
+        depression: true,
+        anxiety: false
+      },
+      {
+        id: 5,
+        student_name: "Laverne W. Murphy",
+        adhd: true,
+        depression: false,
+        anxiety: true
+      },
+      {
+        id: 6,
+        student_name: "Julio Iglesias",
+        adhd: false,
+        depression: false,
+        anxiety: true
+      },
+      {
+        id: 7,
+        student_name: "Frank J. Byard",
+        adhd: true,
+        depression: false,
+        anxiety: true
+      },
+      {
+        id: 8,
+        student_name: "Caleb S. Sun",
+        adhd: true,
+        depression: false,
+        anxiety: true
+      },
+      {
+        id: 9,
+        student_name: "Donna R. Farmer",
+        adhd: true,
+        depression: false,
+        anxiety: true
+      }
+  ]
